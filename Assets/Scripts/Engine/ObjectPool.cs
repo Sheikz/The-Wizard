886a08229f8e0c0a925ee5ef20381de62fc6c378
Sheikz <thomulus@hotmail.com>
2016-08-06 13:21:05 +0200

    Stuff

---
+++ Assets/Scripts/Engine/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour
+{
+    private const int maxSize = 100;
+    private List<SpellController> spellPool;
+
+    void Awake()
+    {
+        spellPool = new List<SpellController>();
+    }
+
+    public void addToPool(SpellController spell)
+    {
+        if (spellPool.Count >= maxSize)
+        {
+            Destroy(spell.gameObject);
+            return;
+        }
+
+        spellPool.Add(spell);
+    }
+}
